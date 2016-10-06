cf5917fad4147b43a5d3db6d58c28c6970946535
andrewkhristyan <andriy.hristyan@yalantis.com>
2016-10-06 13:46:35 +0300

    initial

---
+++ app/src/main/java/client/yalantis/com/githubclient/flow/repository_list/RepositoriesAdapter.kt
@@ -0,0 +1,46 @@
+package client.yalantis.com.githubclient.flow.repository_list
+
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import client.yalantis.com.githubclient.R
+import client.yalantis.com.githubclient.model.Repository
+import kotlinx.android.synthetic.main.item_repository.view.*
+
+/**
+ * Created by andrewkhristyan on 10/5/16.
+ */
+class RepositoriesAdapter(private val repositories: MutableList<Repository>,
+                          val onClick: (Repository) -> Unit)
+: RecyclerView.Adapter<RepositoriesAdapter.ViewHolder>() {
+
+    override fun onBindViewHolder(holder: ViewHolder?, position: Int) {
+        holder?.bindData(repositories[position])
+    }
+
+    override fun getItemCount(): Int = repositories.size
+
+    override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): ViewHolder {
+        return LayoutInflater.from(parent?.context)
+                .inflate(R.layout.item_repository, parent, false).let {
+            ViewHolder(it, onClick)
+        }
+    }
+
+    class ViewHolder(itemView: View, val onClick: (Repository) -> Unit) : RecyclerView.ViewHolder(itemView) {
+
+        fun bindData(repository: Repository) {
+            with(repository) {
+                itemView.text_view_title.text = name
+                itemView.text_view_description.text = description
+                itemView.setOnClickListener { onClick(this) }
+            }
+        }
+    }
+
+
+    fun addRepositories(newRepositories: List<Repository>) {
+        repositories.addAll(newRepositories)
+    }
+}
